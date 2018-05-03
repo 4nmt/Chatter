2a54c1c477b17178dfb52964336f49217335bc37
mthan <thanhanmoc97@gmail.com>
2018-05-03 22:13:29 +0700

    update part 13

--- app/src/main/java/com/example/luffy/chatter/SettingsActivity.java
+++ app/src/main/java/com/example/luffy/chatter/SettingsActivity.java
@@ -1,6 +1,10 @@
 package com.example.luffy.chatter;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.storage.StorageManager;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,6 +12,8 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -15,8 +21,15 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
 import java.util.HashMap;
+import java.util.Random;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -32,8 +45,15 @@ public class SettingsActivity extends AppCompatActivity {
     private TextView mName;
     private TextView mStatus;
 
+    private static final int GALLERY_PICK = 1;
+
+    private StorageReference mImageStorage;
+
+    ProgressDialog loadingBar;
+
     Button btn_change_image;
     Button btn_change_status;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,7 +63,10 @@ public class SettingsActivity extends AppCompatActivity {
         mName = (TextView) findViewById(R.id.setting_display_name);
         mStatus = (TextView) findViewById(R.id.setting_status);
         btn_change_image = (Button) findViewById(R.id.btn_setting_avatar);
-        btn_change_status =(Button) findViewById(R.id.btn_setting_status);
+        btn_change_status = (Button) findViewById(R.id.btn_setting_status);
+
+        mImageStorage = FirebaseStorage.getInstance().getReference();
+
 
         btn_change_status.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -51,7 +74,7 @@ public class SettingsActivity extends AppCompatActivity {
 
                 String status_value = mStatus.getText().toString();
 
-                Intent statusIntent = new Intent(SettingsActivity.this,StatusActivity.class);
+                Intent statusIntent = new Intent(SettingsActivity.this, StatusActivity.class);
                 statusIntent.putExtra("status_value", status_value);
                 startActivity(statusIntent);
             }
@@ -60,6 +83,13 @@ public class SettingsActivity extends AppCompatActivity {
         btn_change_image.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent galleryIntent = new Intent();
+
+                galleryIntent.setType("image/*");
+                galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+
+                startActivityForResult(Intent.createChooser(galleryIntent, "SET IMAGE"), GALLERY_PICK);
+
 
             }
         });
@@ -67,7 +97,7 @@ public class SettingsActivity extends AppCompatActivity {
 
         FirebaseUser current_user = FirebaseAuth.getInstance().getCurrentUser();
         String uid = current_user.getUid();
-
+        mCurrentUser =  current_user;
         mUserDatabase = FirebaseDatabase.getInstance().getReference().child("User").child(uid);
 
         mUserDatabase.addValueEventListener(new ValueEventListener() {
@@ -83,6 +113,8 @@ public class SettingsActivity extends AppCompatActivity {
                 mName.setText(name);
                 mStatus.setText(status);
 
+                Picasso.get().load(image).into(mDisplayImage);
+
             }
 
             @Override
@@ -91,9 +123,63 @@ public class SettingsActivity extends AppCompatActivity {
             }
         });
 
-
-
-
-
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == GALLERY_PICK && resultCode == RESULT_OK) {
+            Uri imageUri = data.getData();
+            CropImage.activity(imageUri)
+                    .setAspectRatio(1, 1)
+                    .start(this);
+        }
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+
+                loadingBar = new ProgressDialog(SettingsActivity.this);
+                loadingBar.setTitle("Đang tải ảnh lên");
+                loadingBar.setMessage("Làm ơn đợi một lát, để chúng tôi xử lý ảnh cho bạn");
+                loadingBar.setCanceledOnTouchOutside(false);
+                loadingBar.show();
+
+
+                Uri resultUri = result.getUri();
+
+                String curr_userid = mCurrentUser.getUid();
+
+                StorageReference filePath = mImageStorage.child("profile_images").child(curr_userid + ".jpg");
+
+                filePath.putFile(resultUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                        if (task.isSuccessful()) {
+
+                            String download_url = task.getResult().getDownloadUrl().toString();
+
+                            mUserDatabase.child("image").setValue(download_url).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        loadingBar.dismiss();
+                                        Toast.makeText(SettingsActivity.this, "Tải ảnh thành công ...", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+
+                        } else {
+                            Toast.makeText(SettingsActivity.this, "Lỗi tải ảnh ... ", Toast.LENGTH_SHORT).show();
+                            loadingBar.dismiss();
+                        }
+                    }
+                });
+
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+            }
+        }
+    }
+
 }
